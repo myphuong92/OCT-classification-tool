73bb62bdec260b719851221e4d1c883746788610
Đoàn Tú Quỳnh <101488005+Quynh-1825@users.noreply.github.com>
2024-06-23 18:44:32 +0700

    commit

--- app.py
+++ app.py
@@ -13,13 +13,10 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/'
 
 model_dirs = [
-    'D:\\Năm 4\\KLTN\\OCT-classification-tool\\static\\models\\densenet201-26.h5',
-    'D:\\Năm 4\\KLTN\\OCT-classification-tool\\static\\models\\efficientnet-b3-29.h5',
-    'D:\\Năm 4\\KLTN\\OCT-classification-tool\\static\\models\\inception_v3-16.h5',
-    'D:\\Năm 4\\KLTN\\OCT-classification-tool\\static\\models\\resnet50-5.h5'
-]
-model_names = [
-    'DenseNet201', 'EfficientNet-B3', 'Inception-V3', 'ResNet50'
+    ['DenseNet201', 'static/models/densenet201-26.h5'],
+    ['EfficientNet-B3', 'static/models/efficientnet-b3-29.h5'],
+    ['Inception-V3', 'static/models/inception_v3-16.h5'],
+    ['ResNet50', 'static/models/resnet50-5.h5']
 ]
 
 mean = np.array([0.19092108, 0.19092108, 0.19092108])
@@ -44,16 +41,92 @@ def model_predict(img_path, model_path):
     
     return prob
 
-ensemble_functions = ['DefaultGompertz', 'Gompertz', 'ShiftGompertz', 'WeightedGompertz', 'Exponential', 'DefaultExponential', 'NewBurr', 'Gamma', 'ExponentialGompertz', 'ExponentialTangent', 'Average']
 
-def fuzzy_rank(CF, top):
+# 1 Gompertz (Go) 
+def DefaultGompertz(x):
+    return 1 - math.exp(-math.exp(-2.0* x))  #Default Gompertz Function
+
+# 3 Exponential (Ex) 
+def DefaultExponential(x): 
+    return 1 - math.exp(-((x-1)**2)/2.0) # Exponential Function
+
+# 2 Exponential * Tangent (ExTan) 
+def DefaultTangent(x):
+    return 1 - math.tanh(((x-1)**2)/2) # DefaultTangent Function
+
+def ExponentialTangent(x):
+    # return (1 - math.exp(-((x-1)**2)/2.0)) * (1 - math.tanh(((x-1)**2)/2)) # exponential tangent
+    return DefaultExponential(x) * DefaultTangent(x) # exponential tangent
+
+# 4 Estimated Gompertz (EGo)
+def Gompertz(x):
+    b = 0.473
+    eta = 1.846
+    #b = 17.5
+    #eta = 0.000001
+    #b = 3.07146
+    return 1 - math.exp(-eta * (math.exp(-b *x)-1))  #Re-paremeter Gompertz Function
+
+# 5 Estimated Weighted Gompertz (EWGo)
+def WeightedGompertz(x):
+    s = 1.97 #1.87
+    l = 0.82275#82275 #
+    #l = 1.8324 #bỏ -1
+    #s= 0.5637 #bỏ -1
+    #s= 1.8755 #
+    #l = 1.837#1.8373 #
+    return 1 - (1+(s * (math.exp(l*x)-1))/(1+l*s))* math.exp(s * (math.exp(l*x)-1)) # Weighted Gompertz
+    #return 1 - (1+(s * (math.exp(l*x)))/(1+l*s))* math.exp(-s * (math.exp(l*x))) # Weighted Gompertz
+    
+# 6 Estimated New Burr (ENB)
+def NewBurr(x):
+    p = 0.0000006156
+    #p = 0.0000009285
+    return (1+ math.exp(- x**3))**(-p) # New Burr Function
+
+# Get penalty với x = 0
+def get_penalty(function):
+    if function =='DefaultGompertz':
+        return DefaultGompertz(0)
+    if function =='ExponentialTangent':
+        return ExponentialTangent(0)
+    if function =='DefaultExponential':
+        return DefaultExponential(0)
+    if function =='Gompertz':
+        return Gompertz(0) 
+    if function =='WeightedGompertz':
+        return WeightedGompertz(0)
+    if function =='NewBurr':
+        return NewBurr(0)
+    
+ensemble_functions = [['DefaultGompertz', 'Gompertz (Go)'], 
+                      ['ExponentialTangent', 'Exponential * Tangent (ExTan)'], 
+                      ['DefaultExponential', 'Exponential (Ex)'], 
+                      ['Gompertz', 'Estimated Gompertz (EGo)'], 
+                      ['WeightedGompertz', 'Estimated Weighted Gompertz (EWGo)'], 
+                      ['NewBurr', 'Estimated New Burr (ENB)']]
+
+def fuzzy_rank(CF, top, function, penalty):
     R_L = np.zeros(CF.shape)
     for i in range(CF.shape[0]):
         for j in range(CF.shape[1]):
             for k in range(CF.shape[2]):
-                R_L[i][j][k] = 1 - math.exp(-math.exp(-2.0*CF[i][j][k]))  #Gompertz Function
-    
-    K_L = 0.632*np.ones(shape = R_L.shape) #initiate all values as penalty values
+                if function =='DefaultGompertz':
+                    R_L[i][j][k] = DefaultGompertz(CF[i][j][k])
+                if function =='ExponentialTangent':
+                    R_L[i][j][k] = ExponentialTangent(CF[i][j][k])
+                if function =='DefaultExponential':
+                    R_L[i][j][k] = DefaultExponential(CF[i][j][k])
+                if function =='Gompertz':
+                    R_L[i][j][k] = Gompertz(CF[i][j][k])
+                if function =='WeightedGompertz':
+                    R_L[i][j][k] = WeightedGompertz(CF[i][j][k])
+                if function =='NewBurr':
+                    R_L[i][j][k] = NewBurr(CF[i][j][k])
+                
+    #default gompertz penalty 0.632
+    K_L = penalty * np.ones(shape = R_L.shape) #initiate all values as penalty values
+    #print ("K_L: ", K_L)
     for i in range(R_L.shape[0]):
         for sample in range(R_L.shape[1]):
             for k in range(top):
@@ -61,33 +134,33 @@ def fuzzy_rank(CF, top):
                 idx = np.where(a==np.partition(a, k)[k])
                 #if sample belongs to top 'k' classes, R_L =R_L, else R_L = penalty value
                 K_L[i][sample][idx] = R_L[i][sample][idx]
-
+    # print ("K_L: ", K_L)
     return K_L
 
-def CFS_func(CF, K_L):
+def CFS_func(CF, K_L, penalty):
     H = CF.shape[0] #no. of classifiers
     for f in range(CF.shape[0]):
         for i in range(CF.shape[1]):
-            idx = np.where(K_L[f][i] == 0.632)
+            idx = np.where(K_L[f][i] == penalty) #default gompertz penalty 0.632
             CF[f][i][idx] = 0
     CFS = 1 - np.sum(CF,axis=0)/H
     return CFS
 
-def Gompertz(top = 2, *argv):
+def Distributions(function, top = 2, *argv):
     L = 0 #Number of classifiers
     for arg in argv:
         L += 1
-
+    
     num_classes = arg.shape[1]
     CF = np.zeros(shape = (L,arg.shape[0], arg.shape[1]))
-
+    penalty = get_penalty(function)
+    # print ("Penalty: ", penalty)
     for i, arg in enumerate(argv):
         CF[:][:][i] = arg
-
-    R_L = fuzzy_rank(CF, top) #R_L is with penalties
-    
+    # print (CF.shape)
+    R_L = fuzzy_rank(CF, top, function, penalty) #R_L is with penalties
     RS = np.sum(R_L, axis=0)
-    CFS = CFS_func(CF, R_L)
+    CFS = CFS_func(CF, R_L, penalty)
     FS = RS*CFS
 
     predictions = np.argmin(FS,axis=1)
@@ -104,27 +177,60 @@ def predict():
         file = request.files['image']
         # Lưu file vào thư mục UPLOAD_FOLDER
         filename = secure_filename(file.filename)
+        filename = filename.split("-")[0] + '.' + filename.split(".")[-1]
+        
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-        outputs = [] 
+        outputs = []
         predicts = []
-        # Dự đoán với mô hình đã tải
-        for model in model_dirs:
-            output = model_predict(file_path, model)  # Lưu kết quả dự đoán vào biến result
-            _, predict = torch.max(output, 1)
-            label = labels[predict.item()]
-            confidence = output[0][predict.item()].item()
-            predicts.append([label, confidence])
-            outputs.append(np.asarray(output[0].tolist()[0:4]))
+        selected_radio = request.form['selected']
         
-        p1 = np.array([outputs[0]])
-        p2 = np.array([outputs[1]])
-        p3 = np.array([outputs[2]])
-        p4 = np.array([outputs[3]])
-        top = 4 #top 'k' classes
-        predictions = Gompertz(top, p1, p2, p3, p4)
-        label = labels[predictions[0]]
-        return render_template('result.html', labels=labels, model_names=model_names, outputs=outputs, predicts=predicts, predictions=label)
+        if selected_radio == 'model':
+            selected_models = request.form.getlist('modelGroup')
+            # Xử lý danh sách các model được chọn
+            for model in model_dirs :
+                if model[0] in selected_models :
+                    output = model_predict(file_path, model[1])
+                    _, predict = torch.max(output, 1)
+                    label = labels[predict.item()]
+                    confidence = output[0][predict.item()].item()
+                    formatConfidence = "{:.2f}%".format(round(confidence * 100, 2))
+                    predicts.append([model[0], label, formatConfidence])
+                    outputs.append(np.asarray(output[0].tolist()[0:4]))
+            
+            return render_template('result.html', labels=labels, outputs=outputs, predicts=predicts, predictions=[])
+    
+        else :
+             # Dự đoán với mô hình đã tải
+            for model in model_dirs:
+                output = model_predict(file_path, model[1])
+                _, predict = torch.max(output, 1)
+                label = labels[predict.item()]
+                confidence = output[0][predict.item()].item()
+                formatConfidence = "{:.2f}%".format(round(confidence * 100, 2))
+                predicts.append([model[0], label, formatConfidence])
+                outputs.append(np.asarray(output[0].tolist()[0:4]))
+        
+            p1 = np.array([outputs[0]])
+            p2 = np.array([outputs[1]])
+            p3 = np.array([outputs[2]])
+            p4 = np.array([outputs[3]])
+            top = 4 #top 'k' classes
+            predictions = []
+            
+            if selected_radio == 'ensemble':
+                selected_ensemble = request.form.getlist('ensembleGroup')
+                # Xử lý danh sách các ensemble được chọn
+                for ensemble in ensemble_functions : 
+                    if ensemble[0] in selected_ensemble :
+                        prediction = Distributions(ensemble[0], top, p1, p2, p3, p4)
+                        predictions.append([ensemble[1], labels[prediction[0]]])
+            else :
+                for ensemble in ensemble_functions : 
+                    prediction = Distributions(ensemble[0], top, p1, p2, p3, p4)
+                    predictions.append([ensemble[1], labels[prediction[0]]])
+        
+            return render_template('result.html', labels=labels, outputs=outputs, predicts=predicts, predictions=predictions)
 
 if __name__ == '__main__':
     app.run()
